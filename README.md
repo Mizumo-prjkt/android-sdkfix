b20ddba89acfd1d7a6041dac4273ee2ddc49d212
Mizumo-prjkt <bluepearl394@gmail.com>
2026-05-29 20:32:12 +0800

    feat: add FUSE OverlayFS service and installation script to resolve Android SDK COW filesystem issues

---
+++ README.md
@@ -0,0 +1,33 @@
+# Android SDK Fix for COW-based Filesystem Problems
+
+This repo contains a fix to address the COW problems for Android SDK, which is not working on my machine.
+
+## The Problem
+This script is not realy innovational. A basic script that shpuld be adressing some issues regarding COW-based filesystems having issues with the Android SDK.
+
+I have tried to solve them by using a script that uses fuse-overlayfs to mount the Android SDK in a way that it is not affected by the COW problems.
+
+## The Fix
+The script uses fuse-overlayfs to mount the Android SDK in a way that it is not affected by the COW problems.
+
+## Installation
+
+### Install fuse-overlayfs
+
+```bash
+sudo pacman -S fuse-overlayfs
+```
+
+## Usage
+
+### Install
+
+```bash
+sudo ./run.sh -i
+```
+
+### Uninstall
+
+```bash
+sudo ./run.sh -r
+```
---
+++ run.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+
+# Files and Directories
+SERVICE="android_sdk_overlay.service"
+TARGET="~/.config/systemd/user/android_sdk_overlay.service"
+
+# .bashrc configuration for android sdk
+BASHRC_TARGET="~/.bashrc"
+
+STRINGS="""
+
+
+# Point Android Home to your new FUSE overlay mount point
+export ANDROID_HOME="$HOME/.local/android/sdk"
+
+# Add the tools to your system PATH so you can run 'sdkmanager' or 'adb' anywhere
+export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools"
+"""
+
+
+# Color
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+NC="\033[0m"
+
+copy() {
+    cp -rf $SERVICE $TARGET
+}
+
+check_and_deploy() {
+    if [ -d "$SYSTEMD_USERDIR" ]
+    then
+        echo -e "$YELLOW[ACTION]$NC: Copying SDK service..."
+        copy
+        if [ $? -eq 0 ]
+        then
+            echo -e "$GREEN[INFO]$NC: Copied SDK service..."
+        else
+            echo -e "$RED[ERROR]$NC: Failed to copy SDK service..."
+            identify_problem -nc
+        fi
+        echo -e "$YELLOW[APPLY ENV]$NC: Applying the environment variables for android sdk..."
+        echo "$STRINGS" >> $BASHRC_TARGET
+        if [ $? -eq 0 ]
+        then
+            echo -e "$GREEN[INFO]$NC: Applied environment variables..."
+        else
+            echo -e "$RED[ERROR]$NC: Failed to apply environment variables..."
+            identify_problem -env
+        fi
+    else
+        echo -e "$RED[ERROR]$NC: Systemd user directory not found..."
+        echo -e "$YELLOW[ACTION]$NC: Investigating..."
+        identify_problem --ns
+    fi
+}
+
+identify_problem() {
+    echo -e "$YELLOW[IDENTIFY]$NC: Checking for possible issues..."
+    if [ "$1" == "--ns" ]
+    then
+        # Lets check for a possible missing systemd user
+        if [ -d "~/.config/systemd/user" ]
+        then
+            echo -e "$GREEN[INFO]$NC: Systemd user directory found..."
+        else
+            echo -e "$RED[ERROR]$NC: Systemd user directory not found..."
+            echo -e "$YELLOW[ACTION]$NC: Creating systemd user directory..."
+            mkdir -p "~/.config/systemd/user"
+            if [ $? -eq 0 ]
+            then
+                echo -e "$GREEN[INFO]$NC: Systemd user directory created..."
+            else
+                echo -e "$RED[ERROR]$NC: Failed to create systemd user directory..."
+                echo -e "Aborting"
+                exit 1
+            fi
+        fi
+    else if [ "$1" == "-nc" ]
+        # Find if android_sdk_overlay.service exists in our current position...
+        if [ -d "$(pwd)/$SERVICE" ]
+        then
+            echo -e "$GREEN[INFO]$NC: Service file found... "
+            echo -e "Still Strange why it isn't located at $TARGET"
+            # Check permissions we have on .config directory
+            if [ -w "~/.config/" ]
+            then
+                echo -e "$GREEN[INFO]$NC: Write permissions on .config directory..."
+                echo -e "$GREEN[INFO]$NC: Advise to redo the whole thing"
+                exit
+            else
+                echo -e "$RED[ERROR]$NC: Write permissions on .config directory..."
+                echo -e "$YELLOW[ACTION]$NC: Please run this script with sudo... $RED(NOT RECOMMENDED)$NC"
+                echo -e "            $YELLOW Or solve permission issues by your own...$NC"
+                exit 1
+            fi
+        else
+            echo -e "$RED[ERROR]$NC: Service file not found..."
+            echo -e "$YELLOW[ACTION]$NC: Please run this script with sudo..."
+            exit 1
+        fi
+    else if [ "$1" == "-env" ]
+        # Check if bashrc exists...
+        if [ -f "$BASHRC_TARGET" ]
+        then
+            echo -e "$GREEN[INFO]$NC: Bashrc file found..."
+        else
+            echo -e "$RED[ERROR]$NC: Bashrc file not found..."
+            echo -e "$YELLOW[ACTION]$NC: Creating bashrc file..."
+            touch "$BASHRC_TARGET"
+            if [ $? -eq 0 ]
+            then
+                echo -e "$GREEN[INFO]$NC: Bashrc file created..."
+            else
+                echo -e "$RED[ERROR]$NC: Failed to create bashrc file..."
+                exit 1
+            fi
+        fi
+    fi
+}
+
+spawn_user_systemd() {
+    # Let SystemD process the overlay using the service file
+    echo -e "$YELLOW[ACTION]$NC: Spawning user systemd to process the overlay..."
+    systemctl --user daemon-reload
+    systemctl --user enable $SERVICE
+    systemctl --user start $SERVICE
+
+    # Check if the service is running
+    if [ $? -eq 0 ]
+    then
+        echo -e "$GREEN[INFO]$NC: Successfully spawned user systemd..."
+        echo -e "$GREEN[INFO]$NC: Done!!!"
+        systemctl --user status android_sdk_overlay.service
+    else
+        echo -e "$RED[ERROR]$NC: Failed to spawn user systemd..."
+        identify_problem --ns
+    fi
+}
+
+
+remove() {
+    # Check if the service is still active
+    # We should stop the service and remove the service file
+    echo -e "$YELLOW[ACTION]$NC: Stopping and removing user systemd..."
+    systemctl --user stop $SERVICE
+    systemctl --user disable $SERVICE
+    rm -f $TARGET
+    systemctl --user daemon-reload
+    echo -e "$GREEN[INFO]$NC: Successfully stopped and removed user systemd..."
+    echo -e "$GREEN[INFO]$NC: Done!!!"
+}
+
+help() {
+    cat << EOF
+    Usage: $0 [OPTIONS]
+    Options:
+        -i    Install user systemd
+        -r    Remove user systemd
+    
+EOF
+}
+
+thread() {
+    # the __main__ function
+    case $1 in 
+        -i)
+            echo -e "$YELLOW[INFO]$NC: Installing user systemd..."
+            check_and_deploy
+            spawn_user_systemd
+            ;; 
+        -r)
+            echo -e "$YELLOW[INFO]$NC: Removing user systemd..."
+            remove
+            ;; 
+        -h | --help)
+            help
+            exit
+            ;;
+        *)
+            echo -e "$RED[ERROR]$NC: Invalid option..."
+            help
+            exit 1
+            ;;
+    esac
+}
+
+thread $1
+
